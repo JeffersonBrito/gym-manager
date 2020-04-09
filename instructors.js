bb503d5773ace8467d8078e40efb892994e00261
Jefferson Brito <jefersonbritoalm@gmail.com>
2020-04-09 15:55:35 -0300

    Create post http

--- instructors.js
+++ instructors.js
@@ -1,3 +1,28 @@
+const fs = require('fs')
+const data = require('./data.json')
+
+//SHOW
+exports.get = function(req, res) {
+  const { id } = req.params
+
+  const foundInstructor = data.instructors.find(function(instructor) {
+    return id == instructor.id
+  })
+
+  if(!foundInstructor) return res.send("Instructor not found!")
+
+  const instructor = {
+    ...foundInstructor,
+    age: "",
+    gender: "",
+    services: "",
+    created_at: "",
+  }
+
+  return res.render('instructors/show', {instructor: foundInstructor})
+}
+
+
 // POST
 exports.post = function(req, res) {
   const keys = Object.keys(req.body)
@@ -8,5 +33,27 @@ exports.post = function(req, res) {
     }
   }
 
-  return res.send(req.body)
+  let {avatar_url, birth, name, services, gender} = req.body
+  
+  req.body.birth = Date.parse(birth)
+  const created_at = Date.now()
+  const id = Number(data.instructors.length + 1)
+
+
+  data.instructors.push({
+    id,
+    name,
+    avatar_url,
+    birth,
+    gender,
+    services,
+    created_at,
+  })
+
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+    if (err) return res.send("Write file error!")  
+
+    return res.redirect("/instructors")
+  })
+  
 }
