c3eaf6819d259e68e5927581812029e345b5bc01
Jefferson Brito <jefersonbritoalm@gmail.com>
2020-04-15 10:21:15 -0300

    Create method put

--- instructors.js
+++ instructors.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const data = require('./data.json')
-const { age } = require('./utils')
+const { age, date } = require('./utils')
 
 //GET
 exports.get = function(req, res) {
@@ -68,5 +68,11 @@ exports.edit = function(req, res) {
 
   if(!foundInstructor) return res.send("Instructor not found!")
 
-  return res.render('instructors/edit', { instructor: foundInstructor })
+  const instructor = {
+    ...foundInstructor,
+    birth: date(foundInstructor.birth)
+  }
+
+
+  return res.render('instructors/edit', { instructor })
 }
